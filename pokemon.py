5bda70537b1eb745ed17e14f6b02148f0f0f69e5
Bruno Costa <bdasilvacosta@gmail.com>
2023-08-29 14:51:24 -0300

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -22,8 +22,10 @@ class Pokemon:
         return f'{self.nome}({self.nivel})'.capitalize()
     
     def atacar(self, pokemon):
-        pokemon.vida = pokemon.vida - self.ataque
-        print(f'{pokemon} perdeu {self.ataque} pontos de vida')
+        ataque_efetivo = int((self.ataque * random.random()* 1.3))
+        pokemon.vida -= ataque_efetivo
+
+        print(f'{pokemon} perdeu {ataque_efetivo} pontos de vida')
 
         if pokemon.vida <= 0:
             print(f'{pokemon} foi derrotado')
