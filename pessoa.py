d718dea4017a1c3edf85417332f9033938aa871f
Bruno Costa <bdasilvacosta@gmail.com>
2023-08-29 16:16:19 -0300

    Update pessoa.py
    resolvido bug na criação dos pokemons do inimigo

--- pessoa.py
+++ pessoa.py
@@ -120,9 +120,13 @@ class Jogador(Pessoa):
 class Inimigo(Pessoa):
     tipo = 'inimigo'
 
-    def __init__(self, nome=None, pokemons=[]):
+    def __init__(self, nome=None, pokemons=None):
         if not pokemons:
+            pokemons_aleatorios = []
             for i in range(random.randint(1, 6)):
-                pokemons.append(random.choice(POKEMONS))
+                pokemons_aleatorios.append(random.choice(POKEMONS))
 
-        super().__init__(nome=nome, pokemons=pokemons)
+            super().__init__(nome=nome, pokemons=pokemons_aleatorios)
+        else:
+            super().__init__(nome=nome, pokemons=pokemons)
+    
